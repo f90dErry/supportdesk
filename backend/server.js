ddffa130cc5e7b52b1ff5dd5008b23dc328f9222
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-04-07 00:50:14 +0000

    handled errors and connected to the database

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
-
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/data')
 const PORT = process.env.PORT || 8000
 
+// connect to database
+connectDB()
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.status.json({ message: 'Welcome to the support desk API' })
 })
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server Started on ${PORT}`))
