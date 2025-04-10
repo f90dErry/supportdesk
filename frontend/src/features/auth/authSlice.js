58fafc8469561d0bd0b1f431fa321b0d16e5cf1e
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-04-10 23:43:29 +0000

    register bug fix
    placed the trycatch block in the login instead of the register

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -13,23 +13,26 @@ const initialState = {
 export const register = createAsyncThunk(
   'auth/register',
   async (user, thunkAPI) => {
-    console.log(user)
+    try {
+      return await authService.response(user)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
   }
 )
 
 // login user
-export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
-  try {
-    return await authService.response(user)
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString()
-
-    return thunkAPI.rejectWithValue(message)
-  }
-})
+export const login = createAsyncThunk(
+  'auth/login',
+  async (user, thunkAPI) => {}
+)
 
 export const authSlice = createSlice({
   name: 'auth',
