37acc1b73d126c72c84311e51693a96b7c66d5b5
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-04-14 14:13:40 +0000

    get single ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -2,10 +2,9 @@ const asyncHandler = require('express-async-handler')
 
 const User = require('../models/userModels')
 const Ticket = require('../models/ticketModel')
-const { status } = require('express/lib/response')
 
-// @desc   Get user ticket
-// @route  GET /api/ticket
+// @desc   Get user tickets
+// @route  GET /api/tickets
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
   // Get user using the id in JWT
@@ -19,6 +18,33 @@ const getTickets = asyncHandler(async (req, res) => {
   const tickets = await Ticket.find({ user: req.user.id })
 
   res.status(200).json(tickets)
+})
+
+// @desc   Get user ticket
+// @route  GET /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.findById(req.params.id)
+
+  if (!ticket) {
+    res.status(404)
+    throw new Error('Ticket not found')
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('Not Authorized')
+  }
+
+  res.status(200).json(ticket)
 })
 
 // @desc   Create new ticket
@@ -52,5 +78,6 @@ const createTicket = asyncHandler(async (req, res) => {
 
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
 }
