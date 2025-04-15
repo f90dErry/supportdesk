39436988f895a76388e6fba325efa38c8ae5f40b
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-04-15 14:56:39 +0000

    added tickets to redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducers from '../features/auth/authSlice'
+import ticketReducer from '../features/tickets/ticketSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducers,
+    ticket: ticketReducer,
   },
 })
 
