49b020d198be19a6e25e24e86dc0fc7e492b14d2
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-04-17 14:16:51 +0000

    fetch notes trough redux

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducers from '../features/auth/authSlice'
 import ticketReducer from '../features/tickets/ticketSlice'
+import noteReducer from '../features/notes/noteSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducers,
     tickets: ticketReducer,
+    notes: noteReducer,
   },
 })
 
