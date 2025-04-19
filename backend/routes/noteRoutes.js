7300fe536a1156f939185cc048daaae4f0f628f2
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-04-19 12:55:11 +0000

    reverted deepseek changes

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router({ mergeParams: true })
 const { getNotes, addNote } = require('../controllers/noteController')
 
 const { protect } = require('../middleware/authMiddleware')
