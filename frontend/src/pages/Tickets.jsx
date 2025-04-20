abebb9f9811714e0a70d953ffc038d722e7a82ff
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2025-04-20 03:04:12 +0000

    Fix case sensitivity issue for TicketItem

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getTickets, reset } from '../features/tickets/ticketSlice'
 import Spinner from '../components/Spinner'
 import BackButton from '../components/BackButton'
-import TicketItem from '../components/TicketItem_temp.jsx'
+import TicketItem from '../components/TicketItem.jsx'
 
 function Tickets() {
   const { tickets, isLoading, isSuccess } = useSelector(
